064040a9ca5b89b39cd4aaa9f635a9be846e37e4
Jeff Breig <jjbreig@gmail.com>
2020-03-22 21:44:52 -0400

    basic CRUD added, need error catching

--- routers/projectsRouter.js
+++ routers/projectsRouter.js
@@ -30,5 +30,39 @@ router.get("/:id", (req, res) => {
     });
 });
 
+router.post("/", (req, res) =>{
+    Projects.insert(req.body)
+    .then(project =>{
+        res.status(201).json(project);
+    })
+    .catch(error => {
+        console.log(error);
+        res.status(500).json({
+            message: 'Error adding project'
+        });
+    });
+});
+
+router.delete('/:id', (req, res) =>{
+    Projects.remove(req.params.id)
+    .then(project =>{
+        res.status(200).json({message: "Project deleted!"});
+    })
+    .catch(error=>{
+        console.log(error)
+        res.status(500).json({message:"There was an error deleting the project"});
+    });
+});
+
+router.put('/:id', (req,res)=>{
+    Projects.update(req.params.id, req.body)
+    .then(project =>{
+        res.status(200).json(project)
+    })
+    .catch(error=>{
+        console.log(error);
+        res.status(500).json({message: 'Error updating the project', error})
+    });
+});
 
 module.exports = router;
